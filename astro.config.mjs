52a12a17e5d9723532e3e96180ab1f3a142d2ba6
Shahidul Islam Majumder <dev@shahid.pro>
2023-05-23 22:24:00 +0600

    feat: add home page layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    react(),
+    mdx(),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,41 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Li Ador Noirrit', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: {
-          50: "#FFEAEE",
-          100: "#FFD4DC",
-          200: "#FFBBC7",
-          300: "#FD97A5",
-          400: "#F3677A",
-          500: "#E03041",
-          600: "#CB1B36",
-          700: "#AC132F",
-          800: "#79122B",
-          900: "#521123",
+          50: '#FFEAEE',
+          100: '#FFD4DC',
+          200: '#FFBBC7',
+          300: '#FD97A5',
+          400: '#F3677A',
+          500: '#E03041',
+          600: '#CB1B36',
+          700: '#AC132F',
+          800: '#79122B',
+          900: '#521123',
         },
         gray: {
-          50: "#F8FAFC",
-          100: "#F1F5F9",
-          200: "#E2E8F0",
-          300: "#CBD5E1",
-          400: "#94A3B8",
-          500: "#64748B",
-          600: "#475569",
-          700: "#334155",
-          800: "#1E293B",
-          900: "#0F172A",
+          50: '#F8FAFC',
+          100: '#F1F5F9',
+          200: '#E2E8F0',
+          300: '#CBD5E1',
+          400: '#94A3B8',
+          500: '#64748B',
+          600: '#475569',
+          700: '#334155',
+          800: '#1E293B',
+          900: '#0F172A',
         },
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
