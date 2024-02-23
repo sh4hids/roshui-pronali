ceedf6bd1d282f951768cb7711c886549b9f6882
Shahidul Islam Majumder <dev@shahid.pro>
2024-02-23 07:36:13 +0600

    feat: add prettier tailwind plugin

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,6 +2,8 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   singleQuote: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   semi: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  pluginSearchDirs: false,
 };
