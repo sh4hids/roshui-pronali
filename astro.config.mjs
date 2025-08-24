9c5ba890a1cc71abdf7ef04e5d5a434fa17557ac
Shahidul Islam Majumder <dev@shahid.pro>
2025-08-24 13:02:59 +0600

    feat: add base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import { remarkModifiedTime } from '@/lib/remarkModifiedTime.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://roshuipronali.shahid.pro/',
+
     vite: {
         plugins: [tailwindcss()],
     },
+
+    integrations: [
+        mdx({
+            rehypePlugins: [remarkModifiedTime],
+        }),
+        react(),
+        sitemap(),
+    ],
 });
+
