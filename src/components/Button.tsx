49d2171b49f88e0f359e1d0dce8d52d559cfae56
Shahidul Islam Majumder <dev@shahid.pro>
2025-12-13 15:30:31 +0600

    fix: refactor design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ export const buttonBaseClasses =
     'cursor-pointer rounded-3xl text-lg uppercase py-4 p-6 drop-shadow hover:drop-shadow-lg ease-in-out duration-300';
 
 export const buttonVariants = {
-    primary: 'bg-chilli-600 hover:bg-chilli-700 text-pablo-50',
+    primary: 'bg-primary hover:bg-chilli-700 text-pablo-50',
     secondary: 'bg-pablo-50 hover:bg-pablo-100 text-chilli-600',
     warning: 'bg-amber-600 hover:bg-amber-700 text-pablo-50',
     error: 'bg-red-600 hover:bg-red-700 text-pablo-50',
